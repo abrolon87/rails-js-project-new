852c37d70b0d645a264214c5f23ec517b8810768
Amanda <main@Amandas-MacBook-Air.local>
2020-07-16 17:14:06 -0400

    builds out problems_controller.rb

---
+++ backend/app/controllers/problems_controller.rb
@@ -0,0 +1,41 @@
+class ProblemsController < ApplicationController
+  
+  def index
+    problems = Problem.all 
+
+    render json: problems, status: 200
+  end
+
+  def show 
+    problem = Problem.find(params[:id])
+
+    render json: problem, status: 200
+  end
+
+  def create
+    problem = Problem.new(problem_params)
+    
+    problem.save 
+    render json: problem, status: 200
+  
+  end
+
+  def update 
+    problem = Problem.find(params[:id])
+    problem.update(problem_params)
+    render json: problem, status: 200
+  end
+
+  def destroy 
+    problem = Problem.find(params[:id])
+    problem.delete
+    render json: {problemId: problem.id}
+  end
+
+  private 
+
+  def problem_params
+    params.require(:problem).permit(:body, :life_aspect_id)
+  end
+
+end
