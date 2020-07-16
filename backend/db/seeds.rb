f01974ccd77b85f75cf6168054c67b9533e9fa4e
Amanda <main@Amandas-MacBook-Air.local>
2020-07-16 18:00:08 -0400

    adds faker gem and seeds.rb

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+requires 'faker'
+
+LifeAspect.delete_all
+Problem.delete_all
+ 
+life_aspects = [
+  'home',
+  'work',
+  'health'
+  
+]
+ 
+life_aspects_array = []
+ 
+life_aspects.each do |name|
+  life_aspects_array << LifeAspect.create(name: name)
+end
+ 
+life_aspects_array.each do |e|
+  e.each do |stuff|
+    body = Faker::Coffee.blend_name
+    date = Faker::Date.between(from: 5.days.ago, to: Date.today)
+    Problem.create(body: body, date: date, life_aspect_id: e.id)
+  end
+end
