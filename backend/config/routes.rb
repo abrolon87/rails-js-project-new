f25efffa3d6d6975cdc0956302c89484c83b2789
Amanda <main@Amandas-MacBook-Air.local>
2020-07-16 17:02:45 -0400

    adds routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :life_aspects
+
+  resources :problems
+
+  resources :life_aspects do 
+    resources :problems   
+  end
+
 end
