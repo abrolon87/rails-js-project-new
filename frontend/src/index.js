68b0e28c6f53b5addd5060c0aca62db5c2042313
Amanda <main@Amandas-MacBook-Air.local>
2020-07-17 00:29:46 -0400

    builds loadLifeAspects and renderLifeAspects

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,43 @@
 const BASE_URL = "http://localhost:3000"
-const TRAINERS_URL = `${BASE_URL}/life_aspects`
-const POKEMONS_URL = `${BASE_URL}/problems`
+const LIFE_ASPECTS_URL = `${BASE_URL}/life_aspects`
+const PROBLEMS_URL = `${BASE_URL}/problems`
+const main = document.querySelector("main")
+
+document.addEventListener("DOMContentLoaded", () => loadLifeAspects())
+
+const loadLifeAspects = () => {
+  const LIFE_ASPECTS_URL = `${BASE_URL}/life_aspects`
+    fetch(LIFE_ASPECTS_URL)
+    .then(result => result.json())
+    .then(json => {
+      json.forEach(life_aspect => renderLifeAspect(life_aspect))
+    })
+
+}
+
+const renderLifeAspect = (lifeAspectHash) => {
+  const div = document.createElement("div")
+  const p = document.createElement("p")
+  const button = document.createElement("button")
+  const ul = document.createElement("ul")
+
+  div.setAttribute("class", "card") //creates cards
+  div.setAttribute("data-id", lifeAspectHash["id"]) // .id
+  //div.setAttribute("class", "card")
+  p.innerHTML = lifeAspectHash.name
+  button.setAttribute("data-life-aspect-id", lifeAspectHash["id"])
+  button.innerHTML = "Add Problem"
+  //attach event listener to button
+
+  div.appendChild(p)
+  div.appendChild(button)
+  div.appendChild(ul)
+
+  main.appendChild(div)
+
+  lifeAspectHash.problems.forEach(problem => renderProblem(problem))
+}
+
+const renderProblem = (problem) => {
+  
+}
