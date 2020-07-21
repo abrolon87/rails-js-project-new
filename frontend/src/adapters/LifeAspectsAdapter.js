1ba7d905b7b092f42a4cd2c9861ad518459ee4cd
Amanda <main@Amandas-MacBook-Air.local>
2020-07-21 00:28:53 -0400

    cleaned up code

--- frontend/src/adapters/LifeAspectsAdapter.js
+++
@@ -1,9 +0,0 @@
-// class LifeAspectsAdapter {
-//   constructor() {
-//   this.baseURL = 'http://localhost:3000/life_aspects'
-//   }
-
-  
-
-
-//}
--- frontend/src/components/app.js
+++
@@ -1,7 +0,0 @@
-// class App {
-//   constructor() {
-//     this.life_aspects = new LifeAspects()
-//     this.problems = new Problems()
-//   }
-// }
-
